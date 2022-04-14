3dfd0b364353278ef3663e567a8088d6ffc570e3
M_Hamid_Zahid <85985817+HamidZahid@users.noreply.github.com>
2022-04-15 00:40:41 +0500

    Extra Files Deleted

--- config.php
+++ config.php
@@ -10,7 +10,3 @@ $conn = mysqli_connect($dbhost, $dbusername, $dbpass, $dbname);
 // }else {
 //     echo "not";
 // }
-/*,'$SelectDegreeType','$DegreeName',
-     '$PassingYear','$Marks','$SelectDegreeTypee','$DegreeName1','$PassingYear1','$Marks1','$SelectDegreeTypeee','$DegreeName2',
-     '$PassingYear2','$Marks2','$SelectDegreeTypeeee','$DegreeName3','$PassingYear3','$Marks3','$EmployeeID','$CompanyDesignation','$EmployeeType',
-     '$BasicSalary','$Salary','$Allowncess','$Amount' */
