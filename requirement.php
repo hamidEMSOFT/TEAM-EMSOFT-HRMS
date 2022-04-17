d4b7b3e0051547858a804f360f860a67352cce35
M_Hamid_Zahid <85985817+HamidZahid@users.noreply.github.com>
2022-04-18 00:41:28 +0500

    RECQUIREMENT.php Added

--- requirement.php
+++ requirement.php
@@ -7,15 +7,102 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+<style>
+  .container2{
+     width: 50%;
+    position: absolute;
+    text-align: left;
+    padding: 10px;
+    align-content: left;
+    margin-left: 30px;
+    margin-top: 30px;
+    border: 2px solid black;
+  }
+ 
+  img{
+    height: 40%;
+     display: flex;
+    position: absolute;
+    margin-left: 700px;
+     margin-top: 200px;
+     
+  }
+</style>
     <title>Hello, world!</title>
   </head>
   <body>
-    
+    <div class="container2" style="width: 600px">
+       <label for="staticEmail" class=" col-form-label"><b>Your company's name*</b></label>
+       <input class="form-control" type="text"    aria-label="default input example">
+       <label for="staticEmail" class=" col-form-label" ><b>Your company's number of employees</b></label>
+<select class="form-control" aria-label=".form-select-sm example">
+  <option selected>Select an option</option>
+  <option value="1">1 to 49</option>
+  <option value="2">50 to 149</option>
+  <option value="3">150 to 299</option>
+  <option value="4">300 to 499</option>
+  <option value="5">500 to 749</option>
+  <option value="6">750 to 999</option>
+  <option value="7">1000+</option>
+</select>
+  <label for="staticEmail" class="  col-form-label"><b>Your first and last name*</b></label>
+    <input class="form-control" type="text"    aria-label="default input example">
+    <label for="staticEmail" class="  col-form-label"><b>Your role in the hiring process</b></label>
+    <select class="form-control" aria-label=".form-select-sm example">
+  <option selected>Select an option</option>
+  <option value="1">Hirirng Manager </option>
+  <option value="2">Recuriter or Talent Acquisition</option>
+  <option value="3">Assistant or Office Manager</option>
+  <option value="4">Human Resources Generalist</option>
+  <option value="5">Owner / CEO </option>
+  <option value="6">Other</option>
+  
+</select>
+<label for="staticEmail" class="  col-form-label"><b>Your typical hiring budget</b></label>
+ <select class="form-control" aria-label=".form-select-sm example">
+  <option selected>Select an option</option>
+  <option value="1">I have a budget for my role(s) </option>
+  <option value="2">No planned budget but I can Spend if needed </option>
+  <option value="3">I'm not able to spend on hiring</option>
+  
+</select>
+   <label for="staticEmail" class="  col-form-label"><b>Primary Reason</b></label>
+    <select class="form-control" aria-label=".form-select-sm example">
+  <option selected>Select an option</option>
+  <option value="1">No budget available for hiring </option>
+  <option value="2">Need spend approval</option>
+  <option value="3">Don't have access to company credit card</option>
+  <option value="3">Not apporved by expense policy</option>
+  <option value="3">Other</option>
+</select>
+  <label for="staticEmail" class="  col-form-label"><b>Your phone number </b></label>
+       <input class="form-control" type="text"    aria-label="default input example">
+<label for="staticEmail" class="  col-form-label"><b>How you heard about us</b></label>
+    <select class="form-control" aria-label=".form-select-sm example">
+  <option selected>Select an option</option>
+  <option value="1">Radio (FM/XM/AM) </option>
+<option value="2">Social Media</option>
+  <option value="3">NewsPaper</option>
+  <option value="4">Podcast</option>
+  <option value="5">Video</option>
+  <option value="6">BillBoard</option>
+  
+  <option value="6">Mail</option>
+  
+  <option value="6">Tv</option>
+  <option value="6">Search engine (ex: google/yahoo/binf)</option>
+  <option value="6">Other</option>
+  
+</select>
+<button type="button" class="btn btn-dark ">Back</button>
+<button type="button" class="btn  btn-primary my-3 mx-5">Save and continue</button>
+</div>
+<div class= "img" style="height:300px">
+ <img src="rec.png" alt="" >
+ </div>
 
-    <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
