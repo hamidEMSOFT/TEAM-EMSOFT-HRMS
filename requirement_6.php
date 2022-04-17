3da8495b3b7a1544daadce4b7a99b035aac41c24
M_Hamid_Zahid <85985817+HamidZahid@users.noreply.github.com>
2022-04-18 02:17:52 +0500

    thk gaya

---
+++ requirement_6.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+        .con{
+            width: 25rem;
+            border: 2px solid black;
+              position: absolute;
+            margin-left: 150px;
+            margin-top: 50px;
+        }
+        .conn{
+            width: 25rem;
+            border: 2px solid black;
+              position: absolute;
+            margin-left: 150px;
+            margin-top: 400px;
+        }
+        .connn{
+            width: 25rem;
+            border: 2px solid black;
+              position: absolute;
+            margin-left: 150px;
+            margin-top: 750px;
+        }
+        .connnn{
+            width: 25rem;
+            border: 2px solid black;
+              position: absolute;
+            margin-left: 150px;
+            margin-top: 1100px;
+        }
+        img{
+    height: 40%;
+     display: flex;
+    position: absolute;
+    margin-left: 700px;
+     margin-top: 200px;
+     
+  }
+    </style>
+    <title>HRMS.com</title>
+  </head>
+  <body>
+      <div class="con">
+    <div class="card" >
+  <div class="card-body">
+    <h5 class="card-title"><b>Electrical Engineer / Project Manager</b></h5>
+    <h6 class="card-subtitle mb-2 text-muted">Mimar Sinan Architect & Contractor</h6>
+    <h6 class="card-subtitle mb-2 text-muted">Company</h6>
+    <h6 class="card-subtitle mb-2 text-muted">Islamabad</h6>
+    <h6 class="card-subtitle mb-2 text-muted">Rs 70,000 - Rs 95,000 a month</h6>
+    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa velit beatae sed ea molestiae itaque error, quas recusandae ex veritatis delectus ipsum totam quos quia excepturi laboriosam fuga, officia reprehenderit earum vel dolor cum suscipit nesciunt sunt. Qui, voluptatum exercitationem?</p>
+    
+  </div>
+</div>
+</div>
+      <div class="connn">
+    <div class="card" >
+  <div class="card-body">
+    <h5 class="card-title"><b>Electrical Engineer / Project Manager</b></h5>
+    <h6 class="card-subtitle mb-2 text-muted">Mimar Sinan Architect & Contractor</h6>
+    <h6 class="card-subtitle mb-2 text-muted">Company</h6>
+    <h6 class="card-subtitle mb-2 text-muted">Islamabad</h6>
+    <h6 class="card-subtitle mb-2 text-muted">Rs 70,000 - Rs 95,000 a month</h6>
+    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa velit beatae sed ea molestiae itaque error, quas recusandae ex veritatis delectus ipsum totam quos quia excepturi laboriosam fuga, officia reprehenderit earum vel dolor cum suscipit nesciunt sunt. Qui, voluptatum exercitationem?</p>
+    
+  </div>
+</div>
+</div>
+      <div class="conn">
+    <div class="card" >
+  <div class="card-body">
+    <h5 class="card-title"><b>Electrical Engineer / Project Manager</b></h5>
+    <h6 class="card-subtitle mb-2 text-muted">Mimar Sinan Architect & Contractor</h6>
+    <h6 class="card-subtitle mb-2 text-muted">Company</h6>
+    <h6 class="card-subtitle mb-2 text-muted">Islamabad</h6>
+    <h6 class="card-subtitle mb-2 text-muted">Rs 70,000 - Rs 95,000 a month</h6>
+    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa velit beatae sed ea molestiae itaque error, quas recusandae ex veritatis delectus ipsum totam quos quia excepturi laboriosam fuga, officia reprehenderit earum vel dolor cum suscipit nesciunt sunt. Qui, voluptatum exercitationem?</p>
+    
+  </div>
+</div>
+</div>
+      <div class="connnn">
+    <div class="card" >
+  <div class="card-body">
+    <h5 class="card-title"><b>Electrical Engineer / Project Manager</b></h5>
+    <h6 class="card-subtitle mb-2 text-muted">Mimar Sinan Architect & Contractor</h6>
+    <h6 class="card-subtitle mb-2 text-muted">Company</h6>
+    <h6 class="card-subtitle mb-2 text-muted">Islamabad</h6>
+    <h6 class="card-subtitle mb-2 text-muted">Rs 70,000 - Rs 95,000 a month</h6>
+    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa velit beatae sed ea molestiae itaque error, quas recusandae ex veritatis delectus ipsum totam quos quia excepturi laboriosam fuga, officia reprehenderit earum vel dolor cum suscipit nesciunt sunt. Qui, voluptatum exercitationem?</p>
+    
+  </div>
+</div>
+</div>
+<div class= "img" style="height:300px">
+  <img src="rec.png" alt="" >
+</div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
