39d89e4f71a0d9df8cd51f52ad79778b4afc4836
M_Hamid_Zahid <85985817+HamidZahid@users.noreply.github.com>
2022-04-17 17:52:07 +0500

    Titles Updated

--- employee.php
+++ employee.php
@@ -13,7 +13,7 @@ $aliyan = mysqli_query($conn, "SELECT* from employee ORDER by id DESC");
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Company Validation</title>
+    <title>Company Employee</title>
   </head>
   <body>
     
